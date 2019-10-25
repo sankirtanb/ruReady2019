89e3c4e2d0d385fae8412ffd3732ab91ee309e6d
Sankirtan <sankirtanb@gmail.com>
2019-10-25 09:25:41 +0200

    added imgsrc

--- mongodb.py
+++ mongodb.py
@@ -21,7 +21,7 @@ def create_user_collection():
 def create_content_collection():
     contents = db["contents"]
     contentDoc = {"contentId": "4164212115654350513",
-                  "title": "California Fires Updates: 50,000 Forced to Evacuate", "body": "Strong wind gusts fanned the flames of a wildfire in northern Los Angeles County on Thursday, forcing the evacuation of at least 50,000 people, the authorities said."}
+                  "title": "California Fires Updates: 50,000 Forced to Evacuate", "body": "Strong wind gusts fanned the flames of a wildfire in northern Los Angeles County on Thursday, forcing the evacuation of at least 50,000 people, the authorities said.", "imgsrc": "https://static01.nyt.com/images/2019/10/24/multimedia/24xp-live-fires/merlin_163260768_807b885c-326d-422b-b178-4b9283209530-videoLarge.jpg"}
     recid = contents.insert_one(contentDoc)
     print("Data inserted with id:", recid)
 
