29c8b2bcda9187a737d07fe8a9031f1414594d02
Krupesh Halavadiya <halvadiya.krupesh@gmail.com>
2019-10-25 07:21:45 +0200

    adding react and node components

---
+++ sample/backend/server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser");
+const path = require("path");
+
+
+
+
+//express app
+const app = express();
+
+
+const dbString = 'mongodb://127.0.0.1/hackathon';
+mongoose.connect(dbString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to mongodb");
+}).catch((err) => {
+  console.log("Something went wrong:  =>" + err);
+});
+
+
+//bodyParser middleware:
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+
+
+
+app.all('*', (req, res, next) => {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+app.use('/', express.static(__dirname + 'frontend'));
+
+
+
+//APIs
+app.get("/", (req, res) => {
+  res.send("Welcome To Backend");
+});
+app.get('/get', (req, res) => {
+  mongoose.find()
+  res.json({
+    "news": [{
+        "title": "Happy birthday, Schatz!: Blake Lively veralbert Ryan Reynolds",
+        "desc": "Was sich liebt, das neckt sich. Und so haben es sich Blake Lively und Ryan Reynolds angewöhnt, sich gegenseitig an ihren Geburtstagen mit unvorteilhaften Bildern auf den Arm zu nehmen. Nun bekommt wieder mal Reynolds sein Fett weg. Und das nicht nur von seiner Frau.",
+        "link": "https://www.n-tv.de/leute/Blake-Lively-veralbert-Ryan-Reynolds-article21351077.html",
+        "imgsrc": "https://bilder4.n-tv.de/img/incoming/crop21351059/3348675873-cImg_4_3-w250/reynolds.jpg"
+      },
+      {
+        "title": "Nach Disqualifikation in Suzuka: Renault verzichtet auf Einspruch und mault",
+        "desc": "Der Renault-Rennstall um den deutschen Piloten Nico Hülkenberg sieht nach der nachträglichen Disqualifikation beider Boliden beim Großen Preis von Japan von einem Einspruch ab. Allerdings versteht Renault nicht, warum ein unter technologischen Gesichtspunkten legales Bremssystems sanktioniert wird.",
+        "link": "https://www.n-tv.de/sport/formel1/Renault-verzichtet-auf-Einspruch-und-mault-article21351335.html",
+        "imgsrc": "https://bilder1.n-tv.de/img/incoming/crop21351324/1718675448-cImg_4_3-w250/imago43382643h.jpg"
+      },
+
+      {
+        "title": "In der Wanne mit Öko-Test: Erkältungsbäder helfen meist nicht weiter",
+        "desc": "In der nasskalten Jahreszeit haben Erkältungsviren Hochkonjunktur. Bei den ersten Symptomen einer Erkrankung schwören viele auf ein heißes Bad mit ätherischen Ölen als Gegenmittel. Doch mal abgesehen vom Wohlfühleffekt hält Öko-Test nicht allzu viel von den Badezusätzen.",
+        "link": "https://www.n-tv.de/ratgeber/Erkaeltungsbaeder-helfen-meist-nicht-weiter-article21350437.html",
+        "imgsrc": "https://bilder2.n-tv.de/img/incoming/crop21350637/8668678408-cImg_4_3-w250/imago94227760h.jpg"
+      }
+
+    ]
+  })
+})
+
+
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Connected to port ${port}`);
+});
---
+++ sample/src/index.jsx
@@ -0,0 +1,366 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+class Game extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      news: []
+    }
+    this.getData()
+  }
+
+  getData() {
+    // create a new XMLHttpRequest
+    var xhr = new XMLHttpRequest()
+
+    // get a callback when the server responds
+    xhr.addEventListener('load', () => {
+      // update the state of the component with the result here
+      let values = xhr.responseText
+      let jsonValues = JSON.parse(values)
+      // arrayToSend = values.news;
+      this.setState({
+        news: jsonValues.news
+      });
+      console.log("array " + JSON.stringify(jsonValues.news));
+    })
+    // open the request with the verb and the url
+    xhr.open('GET', 'http://localhost:5000/get')
+    // send the request
+    xhr.send()
+  }
+
+  render() {
+
+
+
+    // let status;
+    // if (winner) {
+    //   status = 'Winner: ' + winner;
+    // } else {
+    //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    // }
+    console.log("from state " + JSON.stringify(this.state.news));
+    const moves = this.state.news.map((eachnews) => {
+      console.log("eachnews:    ::  " + JSON.stringify(eachnews));
+      return ( <
+        div className = "card" >
+        <
+        img src = {
+          eachnews.imgsrc
+        }
+        className = "card-img-top"
+        alt = "..." / >
+        <
+        div className = "card-body" >
+        <
+        h5 className = "card-title" > {
+          eachnews.title
+        } < /h5> <
+        p className = "card-text" > {
+          eachnews.desc
+        } < /p> <
+        p className = "card-text" > < small className = "text-muted" > Last updated 3 mins ago < /small></p >
+        <
+        /div> < /
+        div >
+      );
+    });
+
+    return (
+      // <div className="game">
+      //   <div className="game-board">
+      //     <Board
+      //       squares={current.squares}
+      //       onClick={(i) => this.handleClick(i)}
+      //     />
+      //   </div>
+      //   <div className="game-info">
+      //     <div>{status}</div>
+      //     <ol>{moves}</ol>
+      //   </div>
+      // </div>
+
+      <
+      div > {
+        moves
+      } < /div>
+      // <
+      // div class = "card" >
+      // <
+      // img src = "https://bilder1.n-tv.de/img/incoming/crop21351324/1718675448-cImg_4_3-w250/imago43382643h.jpg"
+      // class = "card-img-top"
+      // alt = "..." / >
+      // <
+      // div class = "card-body" >
+      // <
+      // h5 class = "card-title" > Card title < /h5> <
+      // p class = "card-text" > This is a wider card with supporting text below as a natural lead - in to additional content.This content is a little bit longer. < /p> <
+      // p class = "card-text" > < small class = "text-muted" > Last updated 3 mins ago < /small></p >
+      // <
+      // /div> < /
+      // div >
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render( <
+  Game / > ,
+  document.getElementById('root')
+);
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+//
+// class Dispatchcard extends React.Component {
+//   constructor() {
+//
+//   }
+//
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+//
+// class Game extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       history: [{
+//         squares: Array(9).fill(null)
+//       }],
+//       stepNumber: 0,
+//       xIsNext: true
+//     };
+//   }
+//
+//   handleClick(i) {
+//     const history = this.state.history.slice(0, this.state.stepNumber + 1);
+//     const current = history[history.length - 1];
+//     const squares = current.squares.slice()
+//     if (calculateWinner(squares) || squares[i]) {
+//       return;
+//     }
+//     squares[i] = this.state.xIsNext ? 'X' : 'O'
+//     this.setState({
+//       history: history.concat([{
+//         squares: squares,
+//       }]),
+//       stepNumber: history.length,
+//       xIsNext: !this.state.xIsNext
+//     })
+//   }
+//
+//   jumpTo(step) {
+//     this.setState({
+//       stepNumber: step,
+//       xIsNext: (step % 2) === 0,
+//     })
+//
+//   }
+//
+//   render() {
+//     const history = this.state.history;
+//     const current = history[this.state.stepNumber];
+//     const winner = calculateWinner(current.squares)
+//
+//     const moves = history.map((step, move) => {
+//       const desc = move ? 'GO TO MOVE: #' + move : 'Go To start'
+//       return ( < li key = {
+//           move
+//         } >
+//         <
+//         button onClick = {
+//           () => this.jumpTo(move)
+//         } > {
+//           desc
+//         } < /button> < /
+//         li > )
+//
+//     })
+//
+//     let status;
+//     console.log(winner);
+//     if (winner) {
+//       console.log("You won! " + winner);
+//       status = "You won! " + winner;
+//     } else {
+//       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+//     }
+//
+//
+//     return ( <
+//       div className = "game" >
+//       <
+//       div className = "game-board" >
+//       <
+//       Board squares = {
+//         current.squares
+//       }
+//       onClick = {
+//         (i) => {
+//           this.handleClick(i)
+//         }
+//       }
+//       / > < /
+//       div > <
+//       div className = "game-info" >
+//       <
+//       div > {
+//         status
+//       } < /div> <
+//       ol > {
+//         moves
+//       } < /ol> < /
+//       div > <
+//       /div>
+//     );
+//   }
+// }
+
+
+//{// class Square extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   // }
+//   render() {
+//     return ( <
+//       button className = "square"
+//       onClick = {
+//         () => {
+//           this.props.onClick()
+//         }
+//       } > {
+//         this.props.value
+//       } <
+//       /button>
+//     );
+//   }
+// }
+//
+//
+//
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6]
+//   ];
+//
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i]
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       console.log("winner winner chicken dinner!");
+//       return squares[a]
+//     }
+//   }
+//   return null;
+// }
+//
+// class Board extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //     squares: Array(9).fill(null),
+//   //     xIsNext: true
+//   //   };
+//   // }
+//
+//
+//
+//   renderSquare(i) {
+//     return <Square value = {
+//       this.props.squares[i]
+//     }
+//     onClick = {
+//       () => {
+//         this.props.onClick(i)
+//       }
+//     }
+//     / > ;
+//   }
+//
+//
+//   render() {
+//     // const winner = calculateWinner(this.props.squares)
+//
+//
+//     return ( < div >
+//       <
+//       div className = "board-row" > {
+//         this.renderSquare(0)
+//       } {
+//         this.renderSquare(1)
+//       } {
+//         this.renderSquare(2)
+//       } < /div> <
+//       div className = "board-row" > {
+//         this.renderSquare(3)
+//       } {
+//         this.renderSquare(4)tps://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+// <script src="htt
+//       } {
+//         this.renderSquare(5)
+//       } <
+//       /div> <
+//       div className = "board-row" > {
+//         this.renderSquare(6)
+//       } {
+//         this.renderSquare(7)
+//       } {
+//         this.renderSquare(8)
+//       } <
+//       /div> < /
+//       div >
+//     );
+//   }
+// }
+//
+//
+//
+//
+// // ========================================
+//
+// ReactDOM.render( <
+// Game / > ,
+//   document.getElementById('root')
+// );
+//
+//
+// // function tick() {
+//   const element = ( < div >
+//     <
+//     h1 > Welcome Sir < /h1> <
+//     h2 > the time here is {
+//       new Date().toLocaleTimeString()
+//     } <
+//     /h2> < /
+//     div >
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+//
+// setInterval(t)
+//}
+*/
